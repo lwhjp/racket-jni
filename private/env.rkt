a9beb6b4e86f040f8d75a714f9a2b4157e568515
Leo Uino <leo@lwh.jp>
2022-02-07 13:46:43 +0900

    Scope and reference wrappers

--- private/env.rkt
+++ private/env.rkt
@@ -1,10 +1,10 @@
 #lang racket/base
 
 (require racket/class
+         ffi/unsafe
          "jni.rkt")
 
-(provide (except-out (all-defined-out)
-                     local-jni-env))
+(provide (all-defined-out))
 
 (define local-jni-env (make-thread-cell #f #f))
 
@@ -33,3 +33,106 @@
 (define-syntax-rule
   (let-jni-env env body0 body ...)
   (with-jni-env env (λ () body0 body ...)))
+
+; scope
+
+(struct jni-scope (refs))
+
+(define (make-jni-scope)
+  (jni-scope (make-ephemeron-hasheq)))
+
+(define (jni-scope-register scope v proc)
+  (hash-set! (jni-scope-refs scope) v proc))
+
+(define (exit-jni-scope scope)
+  (for ([proc (in-hash-values (jni-scope-refs scope))])
+    (proc)))
+
+(define global-jni-scope (make-jni-scope))
+
+(define reference-cleanup-executor (make-will-executor))
+
+(define local-jni-scope
+  (make-thread-cell global-jni-scope #f))
+
+(define (current-jni-scope)
+  (or (thread-cell-ref local-jni-scope)
+      (error "used outside of JNI scope")))
+
+(define (with-jni-scope thunk)
+  (define old-scope #f)
+  (dynamic-wind
+   (λ ()
+     (set! old-scope (thread-cell-ref local-jni-scope))
+     (thread-cell-set! local-jni-scope (make-jni-scope)))
+   thunk
+   (λ ()
+     (exit-jni-scope (thread-cell-ref local-jni-scope))
+     (thread-cell-set! local-jni-scope old-scope)
+     (will-try-execute reference-cleanup-executor (void)))))
+
+; references
+
+(define reference<%>
+  (interface ()
+    ->weak-reference
+    ->local-reference
+    ->global-reference
+    delete
+    get-pointer))
+
+(define reference%
+  (class* object% (reference<%>)
+    (init-field _type)
+    (init pointer)
+    (super-new)
+    (define ptr (box pointer))
+    (define (make-ref ref% p)
+      (and p
+           (new ref%
+                [_type _type]
+                [pointer (cast p _type)])))
+    (define/public (->weak-reference)
+      (make-ref weak-reference%
+                (send (require-jni-env) NewWeakGlobalRef (get-pointer))))
+    (define/public (->local-reference)
+      (make-ref local-reference%
+                (send (require-jni-env) NewLocalRef (get-pointer))))
+    (define/public (->global-reference)
+      (make-ref global-reference%
+                (send (require-jni-env) NewGlobalRef (get-pointer))))
+    (define/public (get-pointer)
+      (or (unbox ptr)
+          (error "invalid reference")))
+    (define/public (clear)
+      (set-box! ptr #f))
+    (define/pubment (delete)
+      (when (unbox ptr)
+        (inner (void) delete)
+        (clear)))))
+
+(define weak-reference%
+  (class* reference% ()
+    (inherit get-pointer)
+    (super-new)
+    (define/override (->weak-reference) this)
+    (define/augment (delete)
+      (send (require-jni-env) DeleteWeakGlobalRef (get-pointer)))))
+
+(define local-reference%
+  (class* reference% ()
+    (inherit get-pointer)
+    (super-new)
+    (jni-scope-register (current-jni-scope) this (λ () (send this clear)))
+    (define/override (->local-reference) this)
+    (define/augment (delete)
+      (send (require-jni-env) DeleteLocalRef (get-pointer)))))
+
+(define global-reference%
+  (class* reference% ()
+    (inherit get-pointer)
+    (super-new)
+    (will-register reference-cleanup-executor this (λ () (send this delete)))
+    (define/override (->global-reference) this)
+    (define/augment (delete)
+      (send (require-jni-env) DeleteGlobalRef (get-pointer)))))
